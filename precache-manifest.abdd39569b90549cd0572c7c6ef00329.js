dc650d673b90651f90f409b959a0e28d451a4884
infisto <dima2992@mail.ru>
2021-09-20 13:51:42 +0300

    first commit

---
+++ precache-manifest.abdd39569b90549cd0572c7c6ef00329.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9eed9a3f71114056e7",
+    "url": "/one-touch/css/app.b8ea6f74.css"
+  },
+  {
+    "revision": "927722dc273d67e01c6f",
+    "url": "/one-touch/css/chunk-4ea1e600.ac3dad76.css"
+  },
+  {
+    "revision": "00c2f101be1c0b982be3",
+    "url": "/one-touch/css/chunk-72a7babc.c7016f7c.css"
+  },
+  {
+    "revision": "09b230b2a4533d147334",
+    "url": "/one-touch/css/chunk-vendors.cdf2a2cc.css"
+  },
+  {
+    "revision": "78012a564fbcd76c78ac708bc94e516d",
+    "url": "/one-touch/img/background1.78012a56.jpg"
+  },
+  {
+    "revision": "048fea58e9cbd071d6e9177553881769",
+    "url": "/one-touch/img/background2.048fea58.jpg"
+  },
+  {
+    "revision": "a5ad2eea19ec2ea39ec65a8c821061d5",
+    "url": "/one-touch/img/bladbarry.a5ad2eea.jpg"
+  },
+  {
+    "revision": "a86e498eb212769a6a8521024f79e689",
+    "url": "/one-touch/img/book.a86e498e.jpg"
+  },
+  {
+    "revision": "06d2ad8c5c2e46dba6955a28be93c658",
+    "url": "/one-touch/img/bulgakov.06d2ad8c.jpg"
+  },
+  {
+    "revision": "7e8ec6dd8c4f4c7fdcfc2a49a4815c31",
+    "url": "/one-touch/img/carrol.7e8ec6dd.jpg"
+  },
+  {
+    "revision": "41cade61ccfed4bf9d1387d650af307b",
+    "url": "/one-touch/img/dante.41cade61.jpg"
+  },
+  {
+    "revision": "938137dbf4a3d9f4ff8956f36bf6cee6",
+    "url": "/one-touch/img/defo.938137db.jpg"
+  },
+  {
+    "revision": "40916b9b92b7a0256e717ab3b9dfce5e",
+    "url": "/one-touch/img/duma.40916b9b.jpg"
+  },
+  {
+    "revision": "2d0cac15e16075288fa42c50bad95e0c",
+    "url": "/one-touch/img/gogol.2d0cac15.jpg"
+  },
+  {
+    "revision": "0d19aa1a4b1e0aa6bdc8d59edd8e0312",
+    "url": "/one-touch/img/gomer.0d19aa1a.jpg"
+  },
+  {
+    "revision": "648997433810fd0a7954b6391040f51a",
+    "url": "/one-touch/img/goncharov.64899743.jpg"
+  },
+  {
+    "revision": "4c9085a43117d3292b645f7a1525b57c",
+    "url": "/one-touch/img/green.4c9085a4.jpg"
+  },
+  {
+    "revision": "8f20ac85d9b9189e4bd0d13aefcace75",
+    "url": "/one-touch/img/gugo.8f20ac85.jpg"
+  },
+  {
+    "revision": "bbe7f0e598de62217a5ca429f8851146",
+    "url": "/one-touch/img/kun.bbe7f0e5.jpg"
+  },
+  {
+    "revision": "5d9e6ccd34dade2683d2961676925f24",
+    "url": "/one-touch/img/oscar.5d9e6ccd.jpg"
+  },
+  {
+    "revision": "01c2e80bbd10c3113c268da63d3c13c9",
+    "url": "/one-touch/img/pushkin.01c2e80b.jpg"
+  },
+  {
+    "revision": "39b7e9e727669f0277498bb61d39c436",
+    "url": "/one-touch/img/vern.39b7e9e7.jpg"
+  },
+  {
+    "revision": "fcc637d8a3e8f6d8abe2ca5b4530759b",
+    "url": "/one-touch/img/woman.fcc637d8.jpg"
+  },
+  {
+    "revision": "adfbe1591ec22c0370298f778253d17d",
+    "url": "/one-touch/index.html"
+  },
+  {
+    "revision": "3f9eed9a3f71114056e7",
+    "url": "/one-touch/js/app.9b41eb8f.js"
+  },
+  {
+    "revision": "927722dc273d67e01c6f",
+    "url": "/one-touch/js/chunk-4ea1e600.6420c3e6.js"
+  },
+  {
+    "revision": "00c2f101be1c0b982be3",
+    "url": "/one-touch/js/chunk-72a7babc.9be82ce8.js"
+  },
+  {
+    "revision": "09b230b2a4533d147334",
+    "url": "/one-touch/js/chunk-vendors.b5c3ac52.js"
+  },
+  {
+    "revision": "9dd5dfdcfcfd65e6710c6bee244c9786",
+    "url": "/one-touch/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/one-touch/robots.txt"
+  }
+]);
